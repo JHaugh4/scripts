31add3c87f693c4152572058bde54199cfe6dd14
Joseph Haugh <jhaugh500@gmail.com>
2026-05-10 00:43:55 -0600

    fix

--- _-rebuild-common.sh
+++ _-rebuild-common.sh
@@ -1,11 +1,11 @@
 machines=(laptop desktop)
 
 pick_machine() {
-    echo "Select a machine:"
+    echo "Select a machine:" >&2
     for i in "${!machines[@]}"; do
-        echo "  $i) ${machines[$i]}"
+        echo "  $i) ${machines[$i]}" >&2
     done
-    printf "Enter number: "
+    printf "Enter number: " >&2
     read -r choice
     if [[ ! "$choice" =~ ^[0-9]+$ ]] || (( choice >= ${#machines[@]} )); then
         echo "Invalid selection" >&2
